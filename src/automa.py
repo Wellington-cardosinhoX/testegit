66615966c99038777a40d5b1c3994ef71d7bebf8
Wellington <wellingtoncardoso1514@gmail.com>
2025-08-26 16:21:16 -0300

    criado mais uma função de multiplicar

--- src/automa.py
+++ src/automa.py
@@ -3,3 +3,10 @@ def soma(x, y):
 
 resultado = soma(3, 5)
 print(f"O resultado da soma é: {resultado}")
+
+
+def multiplica(a, b):
+    return a * b
+
+resultado1 = multiplica(4, 6)
+print(f"O resultado da multiplicação é: {resultado1}")
